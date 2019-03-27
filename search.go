120f8242e825fe2b7843a9bdafc7e34da55790cd
zhandong <1468767640@qq.com>
2019-03-28 01:02:59 +0800

    init search

--- search.go
+++ search.go
@@ -7,11 +7,11 @@ type search struct {
 	notConditions    []map[string]interface{}
 	havingConditions []map[string]interface{}
 	joinConditions   []map[string]interface{}
-	initAttrs        []interface{}
-	assignAttrs      []interface{}
-	selects          map[string]interface{}
-	omits            []string
-	orders           []interface{}
+	//initAttrs        []interface{}
+	//assignAttrs      []interface{}
+	selects map[string]interface{}
+	omits   []string
+	orders  []interface{}
 	//preload          []searchPreload
 	offset           interface{}
 	limit            interface{}
@@ -20,4 +20,84 @@ type search struct {
 	raw              bool
 	Unscoped         bool
 	ignoreOrderQuery bool
+}
+
+func (s *search) clone() *search {
+	clone := *s
+	return &clone
+}
+
+func (s *search) Where(query interface{}, values ...interface{}) *search {
+	s.whereConditions = append(s.whereConditions, map[string]interface{}{"query": query, "args": values})
+	return s
+}
+
+func (s *search) Not(query interface{}, values ...interface{}) *search {
+	s.notConditions = append(s.notConditions, map[string]interface{}{"query": query, "args": values})
+	return s
+}
+
+func (s *search) Or(query interface{}, values ...interface{}) *search {
+	s.orConditions = append(s.orConditions, map[string]interface{}{"query": query, "args": values})
+	return s
+}
+
+func (s *search) Order(value interface{}, reorder ...bool) *search {
+	if len(reorder) > 0 && reorder[0] {
+		s.orders = []interface{}{}
+	}
+	if value != nil && value != "" {
+		s.orders = append(s.orders, value)
+	}
+	return s
+}
+
+func (s *search) Select(query interface{}, args ...interface{}) *search {
+	s.selects = map[string]interface{}{"query": query, "args": args}
+	return s
+}
+
+func (s *search) Omit(columns ...string) *search {
+	s.omits = columns
+	return s
+}
+
+func (s *search) Limit(limit interface{}) *search {
+	s.limit = limit
+	return s
+}
+
+func (s *search) Offset(offset interface{}) *search {
+	s.offset = offset
+	return s
+}
+
+func (s *search) Group(query string) *search {
+	s.group = query
+	return s
+}
+
+func (s *search) Having(query interface{}, values ...interface{}) *search {
+	s.havingConditions = append(s.havingConditions, map[string]interface{}{"query": query, "args": values})
+	return s
+}
+
+func (s *search) Joins(query string, values ...interface{}) *search {
+	s.joinConditions = append(s.joinConditions, map[string]interface{}{"query": query, "args": values})
+	return s
+}
+
+func (s *search) Table(name string) *search {
+	s.tableName = name
+	return s
+}
+
+func (s *search) unscoped() *search {
+	s.Unscoped = true
+	return s
+}
+
+func (s *search) Raw(b bool) *search {
+	s.raw = b
+	return s
 }
